44bfc588103e3be6c4be8e5f0a4da64c2f434014
gudangdubbing <gudangdubbing@gmail.com>
2026-01-20 23:07:03 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
 from pyrogram import Client, filters
+import os
 
-api_id = 36913469
-api_hash = "6e3485759824023ab5b303e4c7271948"
-bot_token = "8564107690:AAFt_ACrM6atWoi6jILHqi64sGi0LOKA0BM"
+api_id = int(os.environ.get("36913469"))
+api_hash = os.environ.get("6e3485759824023ab5b303e4c7271948")
+bot_token = os.environ.get("8564107690:AAFt_ACrM6atWoi6jILHqi64sGi0LOKA0BM")
 
 app = Client(
-    "bot",
+    "streambot",
     api_id=api_id,
     api_hash=api_hash,
     bot_token=bot_token
 )
 
 @app.on_message(filters.command("start"))
-def start(client, message):
-    message.reply("✅ Bot aktif! Siap lanjut.")
+async def start(client, message):
+    await message.reply_text("🤖 Bot aktif!")
 
 app.run()
